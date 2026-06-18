b0b6464e7ab5d3e87853a79a0418ba2b7e9ec007
Ebrahim <smebrahim2012@gmail.com>
2026-06-18 22:56:17 +0500

    implement file reading, chat handling, and shell command execution in graph.py; add main.py for initial output

--- graph.py
+++ graph.py
@@ -3,39 +3,67 @@ from state import AgentState
 from groq import Groq
 from utils import read_file
 from dotenv import load_dotenv
+import subprocess
 
 load_dotenv()
 
 client = Groq()
+
+def readFileNode(state):
+    parts = state["command"].split(maxsplit=1)
+
+    if len(parts) < 2:
+        return {"response": "No file specified"}
+    
+    return {
+        "response": read_file(parts[1].strip())
+    }
+
+def chatNode(state):
+
+    completion = client.chat.completions.create(
+            model='llama-3.3-70b-versatile',
+            messages=[{"role": "user", "content": state["command"]}]
+        )
+
+    return {"response": completion.choices[0].message.content}
+
+def runShellNode(state):
+    try:
+        result = subprocess.run(
+            state["command"][4:].strip(),
+            shell=True,
+            capture_output=True,
+            text=True
+        )
+        return {"response": result.stdout}
+    
+    except Exception as e:
+        return {"response": f"Error occurred: {str(e)}"}
 
 def planner(state):
     command = state["command"].lower()
 
     if command.startswith("read "):
         return {"action": "read_file"}
+    
+    elif command.startswith("run "):
+        return {"action": "run_shell"}
 
     return {"action": "chat"}
+
 
 def executor(state):
     if state['action'] == "read_file":
 
-        parts = state["command"].split(maxsplit=1)
-
-        if len(parts) < 2:
-            return {"response": "No file specified"}
-        
-        return {
-            "response": read_file(parts[1].strip())
-        }
+        return readFileNode(state)
     
     elif state['action'] == "chat":
 
-        completion = client.chat.completions.create(
-            model='llama-3.3-70b-versatile',
-            messages=[{"role": "user", "content": state["command"]}]
-        )
+        return chatNode(state)
 
-        return {"response": completion.choices[0].message.content}
+    elif state['action'] == "run_shell":
+        return runShellNode(state)
 
     else:
         return {
